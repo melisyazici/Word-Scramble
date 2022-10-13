c821fd62117088cb823d067a94827b84817972eb
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-13 23:17:37 +0200

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Melis Yazıcı on 13.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
