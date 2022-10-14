37c1db50caaff3b2e1308db7898917814b6df8a6
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-14 16:18:00 +0200

    Configure the cells in the table view

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 
 
